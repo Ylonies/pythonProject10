b4efe76c612d6bf95349fc3066b2bff2b069ae13
Zhanna <nurbekovna@mail.ru>
2022-04-04 18:23:30 +0700

    Добавляем капитана

--- data/jobs.py
+++ data/jobs.py
@@ -12,12 +12,11 @@ class News(SqlAlchemyBase):
                            primary_key=True, autoincrement=True)
     team_leader = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("users.id"))
     job = sqlalchemy.Column(sqlalchemy.String)
-
-    title = sqlalchemy.Column(sqlalchemy.String)
+    work_size = sqlalchemy.Column(sqlalchemy.Integer)
     collaborators = sqlalchemy.Column(sqlalchemy.String)
 
-    start_date = sqlalchemy.Column(sqlalchemy.DateTime)
-    end_date = sqlalchemy.Column(sqlalchemy.DateTime)
+    start_date = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.datetime.now)
+    end_date = sqlalchemy.Column(sqlalchemy.DateTime, nullable=True)
     is_finished = sqlalchemy.Column(sqlalchemy.Boolean)
 
     user = orm.relation('User')
