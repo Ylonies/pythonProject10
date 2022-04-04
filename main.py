253f814ef0781d7d142bd2bb5ab77c138a6bbe02
Zhanna <nurbekovna@mail.ru>
2022-04-04 17:48:09 +0700

    Добавляем капитана

--- main.py
+++ main.py
@@ -7,9 +7,29 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
+def add_user(params):
+
+    user = User()
+    user.surname = params[0]
+    user.name = params[1]
+    user.age = params[2]
+    user.position = params[3]
+    user.speciality = params[4]
+    user.address = params[5]
+    user.email = params[6]
+    user.hashed_password = params[7]
+    db_sess = db_session.create_session()
+    db_sess.add(user)
+    db_sess.commit()
+
 def main():
     db_session.global_init("db/mars_explorer.db")
+    add_user(["Scott", "Ridley", 21, "captain", "research engineer", "module_1", "scott_chief@mars.org", "cap"])
+    add_user(["Green", "Lucy", 30, "captain helper", "research engineer", "module_1", "lucy_thebest@mars.org", "best"])
+    add_user(["Black", "Saimon", 25, "usual worker", "scientist", "module_1", "black@mars.org", "saimon"])
+    add_user(["Gagarina", "Polina", 50, "usual worker", "chief", "module_1", "gagrin@mars.org", "space"])
     app.run()
+
 
 if __name__ == '__main__':
     main()
