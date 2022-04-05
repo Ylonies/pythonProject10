d100b1d34ca3f75a59716e96848ef5d63aa6a8b5
Zhanna <nurbekovna@mail.ru>
2022-04-05 19:19:46 +0700

    Форма регистрации

--- main.py
+++ main.py
@@ -4,31 +4,47 @@ from werkzeug.utils import redirect
 from data import db_session
 from data.jobs import Jobs
 from data.users import User
+from forms.users import RegisterForm
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
-
-
-def add_user(params):
-
-    user = User()
-    user.surname = params[0]
-    user.name = params[1]
-    user.age = params[2]
-    user.position = params[3]
-    user.speciality = params[4]
-    user.address = params[5]
-    user.email = params[6]
-    user.hashed_password = params[7]
-    db_sess = db_session.create_session()
-    db_sess.add(user)
-    db_sess.commit()
 
 @app.route("/")
 def main():
     db_session.global_init("db/mars_explorer.db")
     db_sess = db_session.create_session()
     jobs = db_sess.query(Jobs).all()
-    return render_template("index.html", jobs = jobs)
+    return render_template("index.html", jobs=jobs)
+
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            name=form.name.data,
+            email=form.email.data,
+            surname=form.surname.data,
+            age=form.age.data,
+            position=form.position.data,
+            speciality=form.speciality.data,
+            address=form.address.data
+
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
+
 
 if __name__ == '__main__':
     app.run(port=5000, host='127.0.0.2')
